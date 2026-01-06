5c65551e22c3bfc30573b9088df5a88c7a54868a
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-06 23:20:17 +0545

    initial commit for the project

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [{ hostname: 'lh3.googleusercontent.com' }],
+  },
 };
 
 export default nextConfig;
