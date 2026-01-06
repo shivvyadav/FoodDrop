8ff6fb3a70722e945b135e0010f2be393d22c6a6
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-06 23:25:18 +0545

    add mongoose and user model

---
+++ src/models/User.ts
@@ -0,0 +1,65 @@
+import mongoose, { Schema } from 'mongoose';
+
+export interface IUser {
+  username: string;
+  email: string;
+  password?: string;
+  contact?: number;
+  role: 'admin' | 'user' | 'delivery';
+  image?: string;
+  forgotPasswordToken?: String;
+  forgotPasswordTokenExpiry?: Date;
+  verifyToken?: String;
+  verifyTokenExpiry?: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      lowercase: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: false,
+    },
+    contact: {
+      type: Number,
+      required: false,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'delivery', 'admin'],
+      default: 'user',
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordTokenExpiry: {
+      type: Date,
+    },
+    verifyToken: {
+      type: String,
+    },
+    verifyTokenExpiry: {
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.models?.User || mongoose.model<IUser>('User', userSchema);
+export default User;
