b54290cd6efafa7d292af3926d10af5592db89c9
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-08 23:17:41 +0545

    created the route for the admin for adding food items

---
+++ src/app/api/admin/add-food/route.ts
@@ -0,0 +1,63 @@
+import connectDB from '@/lib/db';
+import { NextRequest, NextResponse } from 'next/server';
+import { auth } from '@/auth';
+import uploadOnCloudinary from '@/lib/cloudinary';
+import Food from '@/models/Food';
+
+type Food = {
+  name: string;
+  image: File | null;
+  price: number;
+  category: string;
+  quantity: number;
+};
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const session = await auth();
+    if (session?.user.role !== 'admin') {
+      return NextResponse.json(
+        { success: false, message: 'Unauthorized access' },
+        { status: 401 },
+      );
+    }
+
+    const formData = await req.formData();
+
+    const name = formData.get('name') as string;
+    const price = Number(formData.get('price'));
+    const category = formData.get('category') as string;
+    const quantity = Number(formData.get('quantity'));
+    const image = formData.get('image') as File | null;
+
+    if (!name || !image || !price || !category || !quantity) {
+      return NextResponse.json(
+        { success: false, message: 'All fields are required' },
+        { status: 400 },
+      );
+    }
+
+    const imageUrl = await uploadOnCloudinary(image);
+
+    const newFood = await Food.create({
+      name,
+      image: imageUrl,
+      price,
+      category,
+      quantity,
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: 'Food added successfully',
+      data: newFood,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, message: 'Internal server error' },
+      { status: 500 },
+    );
+  }
+}
