1e06d0d3b586902e035fde16b52089bffb34045a
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-08 23:22:46 +0545

    model for the food items

---
+++ src/models/Food.ts
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose';
+
+interface IFood {
+  name: string;
+  image: string;
+  price: number;
+  category: string;
+  quantity: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const foodSchema = new mongoose.Schema<IFood>(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    image: {
+      type: String,
+      required: [true, 'Image is required'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Price is required'],
+    },
+    category: {
+      type: String,
+      enum: [
+        'Pizza',
+        'Burgers',
+        'Momos',
+        'Chowmein',
+        'Chatpate',
+        'Pani Puri',
+        'Biryani',
+        'Chinese',
+        'Meals',
+        'Fast Food',
+        'Beverages',
+        'Desserts',
+        'Snacks',
+        'Salads',
+        'Bakery',
+        'Ice Cream',
+        'Drinks',
+      ],
+      required: [true, 'Category is required'],
+    },
+    quantity: {
+      type: Number,
+      required: [true, 'Quantity is required'],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Food = mongoose.models.Food || mongoose.model<IFood>('Food', foodSchema);
+export default Food;
