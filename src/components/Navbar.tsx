14ae43618de890dd223f62005c9831979d86dc85
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-11 12:45:44 +0545

    make some changes to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,5 @@
 'use client';
-
-import fooddroplogo from '@/assets/fooddroplogo.png';
 import { motion, MotionConfig } from 'motion/react';
-import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
 import AnimatedHamburgerButton from './ui/AnimatedHamburgerButton';
