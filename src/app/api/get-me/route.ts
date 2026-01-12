a94eb440d5b7fbf4eaefe1c10e900e5bfb430a15
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-12 09:19:24 +0545

    add the get- me api to fetch the user data

---
+++ src/app/api/get-me/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { auth } from '@/auth';
+import connectDB from '@/lib/db';
+import User from '@/models/User';
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+    const session = await auth();
+    if (!session || !session?.user) {
+      return NextResponse.json(
+        { success: false, message: 'Unauthorized access' },
+        { status: 401 },
+      );
+    }
+    const user = await User.findById(session.user.id).select('-password');
+    if (!user) {
+      return NextResponse.json(
+        { success: false, message: 'User not found' },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json({ success: true, user }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
