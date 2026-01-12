4cc6d846b82fbc2e4b08cd7e274a81600dc7f401
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-12 09:19:55 +0545

    add the route to fetch the foods data from the database

---
+++ src/app/api/get-foods/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+import connectDB from '@/lib/db';
+import Food from '@/models/Food';
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+    const foods = await Food.find({}).lean();
+    if (!foods) {
+      return NextResponse.json(
+        { success: false, message: 'No foods found' },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json({ success: true, foods }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
