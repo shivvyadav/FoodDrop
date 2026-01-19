8b9d0a085055e175590583b2ea1bdd0984af3848
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-19 20:39:05 +0545

    handler for emitting event for real time activity

---
+++ src/lib/emitEventHandler.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export default async function emitEventHandler(
+  event: string,
+  data: any,
+  socketId?: string,
+) {
+  try {
+    await axios.post(`${process.env.NEXT_PUBLIC_SOCKET_URL}/notify`, {
+      socketId,
+      event,
+      data,
+    });
+  } catch (error) {}
+}
