ed24465f6a19cca8d582636a7080f1a3a7bd615e
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-21 23:21:50 +0545

    add the route to fetch current order for delivery boy

---
+++ src/app/api/delivery/current-order/route.ts
@@ -0,0 +1,44 @@
+import { auth } from '@/auth';
+import connectDB from '@/lib/db';
+import Order from '@/models/Order';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+    const session = await auth();
+
+    // check if user is authenticated
+    if (!session || !session?.user) {
+      return NextResponse.json(
+        { success: false, message: 'Unauthorized access' },
+        { status: 401 },
+      );
+    }
+
+    const deliveryBoyId = session.user.id;
+
+    // check if assignment exists
+    const activeAssignment = await Order.findOne({
+      assignedDeliveryBoy: deliveryBoyId,
+      status: 'out for delivery',
+    }).lean();
+
+    if (!activeAssignment) {
+      return NextResponse.json(
+        { success: false, message: 'No orders found' },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, activeAssignment },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: `Internal Server Error :${error}` },
+      { status: 500 },
+    );
+  }
+}
