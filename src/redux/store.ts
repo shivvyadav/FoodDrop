a132b80294c46f976baf645a7e85b0a79b3f2236
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-24 23:13:02 +0545

    add the other slices to the store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './slices/userSlice';
 import foodSlice from './slices/foodSlice';
 import cartSlice from './slices/cartSlice';
+import orderSlice from './slices/orderSlice';
+import allOrderSlice from './slices/allOrderSlice';
+import deliverySlice from './slices/deliverySlice';
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
     food: foodSlice,
     cart: cartSlice,
+    order: orderSlice,
+    allOrder: allOrderSlice,
+    delivery: deliverySlice,
   },
 });
 
