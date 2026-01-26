debdc0db4a01c5678b2a336d2a01dc738983ec37
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-26 23:41:59 +0545

    Initialization file

---
+++ src/Init/InitDeliveryBoySocket.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import useDeliveryBoySocket from '@/hooks/socket/useDeliveryBoySocket';
+export default function InitDeliveryBoySocket() {
+  useDeliveryBoySocket();
+  return null;
+}
