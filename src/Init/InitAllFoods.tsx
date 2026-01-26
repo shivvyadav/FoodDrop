a503d77db6514296ecf2b553a41bd7c4fa7987e2
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-26 23:41:19 +0545

    Initialization file

---
+++ src/Init/InitAllFoods.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import useGetFoods from '@/hooks/useGetFoods';
+export default function InitAllFoods() {
+  useGetFoods();
+
+  return null;
+}
---
+++ src/Init/InitAllOrders.ts
@@ -0,0 +1,8 @@
+'use client';
+
+import useGetAllOrders from '@/hooks/useGetAllOrders';
+export default function InitAllOrders() {
+  useGetAllOrders();
+
+  return null;
+}
