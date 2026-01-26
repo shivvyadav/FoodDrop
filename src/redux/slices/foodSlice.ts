dbb9dae5816fb21fd70a916c7fb1273e6935bae4
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-26 23:42:37 +0545

    update the food slice

--- src/redux/slices/foodSlice.ts
+++ src/redux/slices/foodSlice.ts
@@ -2,10 +2,12 @@ import { createSlice } from '@reduxjs/toolkit';
 import { IFood } from '@/models/Food';
 
 interface foodState {
-  foodData: IFood[] | [];
+  foodData: IFood[];
+  isLoaded: boolean;
 }
 const initialState: foodState = {
   foodData: [],
+  isLoaded: false,
 };
 
 export const foodSlice = createSlice({
@@ -14,9 +16,28 @@ export const foodSlice = createSlice({
   reducers: {
     setFoodData: (state, action) => {
       state.foodData = action.payload;
+      state.isLoaded = true;
+    },
+    prependFoodData: (state, action) => {
+      state.foodData.unshift(action.payload);
+    },
+
+    updateFoodData: (state, action) => {
+      state.foodData = state.foodData.map((food) => {
+        if (food._id?.toString() === action.payload._id?.toString()) {
+          return action.payload;
+        }
+        return food;
+      });
+    },
+    deleteFoodData: (state, action) => {
+      state.foodData = state.foodData.filter(
+        (food) => food._id?.toString() !== action.payload?.toString(),
+      );
     },
   },
 });
 
-export const { setFoodData } = foodSlice.actions;
+export const { setFoodData, prependFoodData, updateFoodData, deleteFoodData } =
+  foodSlice.actions;
 export default foodSlice.reducer;
