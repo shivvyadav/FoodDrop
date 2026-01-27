ae7649e4f51a8d736de18775ce726d2c3d9fefcd
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-27 23:50:39 +0545

    update the file

--- src/components/Admin/AdminNav.tsx
+++ src/components/Admin/AdminNav.tsx
@@ -2,13 +2,11 @@ import { auth } from '@/auth';
 import Logo from '../ui/Logo';
 import AdminMenu from './AdminMenu';
 import ProfileMenu from '../ProfileMenu';
-import GeoLocationUpdater from '../GeoLocationUpdater';
 export default async function AdminNav() {
   const session = await auth();
 
   return (
     <div className="fixed top-0 left-0 z-30 flex h-18 w-full items-center justify-between bg-white px-4 shadow lg:px-16">
-      {/* <GeoLocationUpdater userId={session?.user.id} /> */}
       <Logo />
       <div className="flex items-center gap-4">
         <AdminMenu user={session?.user} />
