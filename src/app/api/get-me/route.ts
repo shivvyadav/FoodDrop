bc1dc3b5549e66448ddf0ef7e9e014b7d14cd21a
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-28 23:58:42 +0545

    edit the get me route

--- src/app/api/get-me/route.ts
+++ src/app/api/get-me/route.ts
@@ -23,7 +23,7 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ success: true, user }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
-      { success: false, message: 'Internal Server Error' },
+      { success: false, message: `Internal server error: ${error}` },
       { status: 500 },
     );
   }
