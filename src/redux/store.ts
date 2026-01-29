d9aa97049dd4e41a69bc960dd6b4a66355af488b
Shiv Yadav <shivyadav0014@gmail.com>
2026-01-29 22:25:48 +0545

    add the redux persist to persist the cart items

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userSlice from './slices/userSlice';
 import foodSlice from './slices/foodSlice';
 import cartSlice from './slices/cartSlice';
@@ -6,16 +18,31 @@ import orderSlice from './slices/orderSlice';
 import allOrderSlice from './slices/allOrderSlice';
 import deliverySlice from './slices/deliverySlice';
 
+const cartPersistConfig = {
+  key: 'cart',
+  storage,
+};
+
+const persistedCartReducer = persistReducer(cartPersistConfig, cartSlice);
+
 export const store = configureStore({
   reducer: {
     user: userSlice,
     food: foodSlice,
-    cart: cartSlice,
+    cart: persistedCartReducer,
     order: orderSlice,
     allOrder: allOrderSlice,
     delivery: deliverySlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
